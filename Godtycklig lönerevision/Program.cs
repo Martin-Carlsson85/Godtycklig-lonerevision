0b536de77246bb47f38de7a1ad4bd0ad21f4219b
mc22dg <mc22dg@student.lnu.se>
2014-09-22 09:59:20 +0200

    Lagt till kod.

--- Godtycklig lönerevision/Program.cs
+++ Godtycklig lönerevision/Program.cs
@@ -14,10 +14,83 @@ namespace Godtycklig_lönerevision
 
 
         }
-        static int ReadInt()
+        
+        static int ReadInt(string prompt)
         {
+            int total1 = 0;
+            int total2 = 0;
 
-            return 8;
+            while (true)
+            {
+                try
+                {   
+                    
+                    //Försöker översätta inmatat värde till en int-variabel.
+                    Console.Write("Ange antal löner att mata in:");
+                    total1 = int.Parse(Console.ReadLine());
+
+                    break;
+
+                }
+
+
+                catch( FormatException )
+                {   
+                    
+                    //Ifall det inte fungerade så kommer detta att köras istället.
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nFEL! Det du angivet är felaktigt.");
+                    Console.ResetColor();
+                }
+
+            for (int i=0; i<1000; i++ )
+            {
+
+
+            }
+
+
+              while (true)
+                {
+                
+                  try
+                    {   
+                      
+                      
+                    //Försöker översätta inmatat värde till en int-variabel.
+                    Console.Write("Ange lön nummer 1:");
+                    total2 = int.Parse(Console.ReadLine());
+
+                    break;
+
+                    }
+
+
+                catch( FormatException )
+                {   
+                    
+                    //Ifall det inte fungerade så kommer detta att köras istället.
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nFEL! Det du angivet är felaktigt.");
+                    Console.ResetColor();
+                }
+            
+            
+            
+            
+            
+            
+            
+            }
+
+            
         }
+
+
+
+    }
+}
+
+
     }
 }
